3d5b93f520fe4d627d6d8947465f4c513afa6c18
chenyulong <cyl306008324@gmail.com>
2017-04-14 10:06:31 +0800

    test edit file to git

--- obfuscatetest/src/main/java/xaircraft/obfuscatetest/MainActivity.java
+++ obfuscatetest/src/main/java/xaircraft/obfuscatetest/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "click btn", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "click btn", Toast.LENGTH_SHORT).show();
             }
         });
     }
