13b42a82031590a0d0552bc2719750fa25505ddd
chenyulong <cyl306008324@gmail.com>
2017-04-14 10:00:38 +0800

    test add file to git

---
+++ customview/src/main/java/xaircraft/customview/view/ProgressPie.java
@@ -0,0 +1,22 @@
+package xaircraft.customview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by chenyulong on 15/12/14.
+ */
+public class ProgressPie extends View {
+    public ProgressPie(Context context) {
+        super(context);
+    }
+
+    public ProgressPie(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProgressPie(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
